8ed7086e6d5e8cc01bd5b379caa4be72874f00ba
Guillermo García <guillermo.garcia@ts-iberia.com>
2019-03-28 19:00:32 +0100

    logger creado

--- src/Controller/Kernel.php
+++ src/Controller/Kernel.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace PPR\Controller;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
 
 /*
 *Kernel
@@ -10,11 +12,13 @@ class Kernel
 {
     private $router;
     private $request;
+    private $logger;
 
     function __construct()
     {
         $this->router = new Router();
         $this->request = new Request();
+        $this->logger = new Logger('ppr');
 
     }
 
@@ -22,6 +26,8 @@ class Kernel
     {
         $this->router->start_router();
         $this->router->parse_route($this->request);
+        $this->logger->pushHandler(new StreamHandler(__DIR__.'/../../var/log/ppr_log.log', Logger::DEBUG));
+        $this->logger->info("Kernel ha sido lanzado");
     }
 }
 
--- src/Controller/Router.php
+++ src/Controller/Router.php
@@ -23,8 +23,22 @@ class Router
     public function parse_route($request)
     {
         $this->url = str_replace($this::URL_START_DELETE,"",$this->url);
-        $explode_url=explode("/",$this->url); 
-        var_dump($explode_url);
+        $explode_url=explode("/",$this->url);
+        if(count($explode_url)==1)
+        {
+            $request->setController($explode_url[0]);
+            $request->setAction("index");
+        }elseif (count($explode_url)==2) {
+            $request->setController($explode_url[0]);
+            $request->setAction($explode_url[1]);
+        }
+        elseif (count($explode_url)>2) {
+            $request->setController($explode_url[0]);
+            $request->setAction($explode_url[1]);
+            $request->setParams(array_slice($explode_ur,2));
+        }
+
+        var_dump($request);
     }
 }
 
