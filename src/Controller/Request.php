0c2e58f280ceed8a3414d7e12109ff7657f8d951
1daw <1daw@WINMAC18.alumnado.flo>
2019-03-28 11:05:45 +0100

    request y router casi

--- src/Controller/Request.php
+++ src/Controller/Request.php
@@ -8,10 +8,51 @@ namespace PPR\Controller;
 
 class Request
 {
+    private $controller=null;
+    private $action=null;
+    private $params=null;
+
     function __construct()
     {
         echo"Lanzamos Request";
     }
+
+    public function getController()
+    {
+        return $this->controller;
+    }
+    
+    public function getAction()
+    {
+        return $this->action;
+    }
+
+    public function getParams()
+    {
+        return $this->params;
+    }
+
+    public function getParams()
+    {
+        return $this->params;
+    }
+
+    public function setController($controller)
+    {
+        $this->controller = $controller;
+    }
+
+    public function setAction($action)
+    {
+        $this->action = $action;
+    }
+
+    public function setParams($params)
+    {
+        $this->params = $params;
+    }
+
+    
 }
 
 ?>
