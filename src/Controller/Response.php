d23a62bba80ad58c034dd56549e9d96fdfff7c27
Guillermo García <guillermo.garcia@ts-iberia.com>
2019-03-29 19:06:20 +0100

    Response creado

---
+++ src/Controller/Response.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace namespace PPR\Controller;
+
+/*
+*Response
+*/
+
+class Response
+{
+    private $content;
+
+    function __construct()
+    {
+        $this->content="";
+    }
+
+
+    function getContent()
+    {
+        return $this->content;
+    }
+
+    function setContent($content)
+    {
+        $this->content = $content;
+    }
+}
+
+?>
