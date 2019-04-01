5ea74a963b1289ae1ccc446789db176263a73ce0
WilliamGallifrey <guigarro23@gmail.com>
2019-04-01 10:15:08 +0200

    router cambios

--- src/Controller/Router.php
+++ src/Controller/Router.php
@@ -35,7 +35,7 @@ class Router
         elseif (count($explode_url)>2) {
             $request->setController($explode_url[0]);
             $request->setAction($explode_url[1]);
-            $request->setParams(array_slice($explode_ur,2));
+            $request->setParams(array_slice($explode_url,2));
         }
 
         var_dump($request);
