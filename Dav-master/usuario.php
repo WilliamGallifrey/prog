a81b7bfee6dc53da7a27829360c363e998cd57d8
davidvic99 <43604557+davidvic99@users.noreply.github.com>
2019-04-14 19:51:48 +0200

    Login y registro creados, Falta probar login

--- Dav-master/usuario.php
+++ Dav-master/usuario.php
@@ -31,4 +31,29 @@ class usuario extends conexion{
 
     }
 
+
+    public function loginUsuario($conexion){
+
+    if (isset($_POST['usuario'])){
+        $username = stripslashes($_REQUEST['usuario']);
+        $username = mysqli_real_escape_string($conexion,$username);
+        $password = password_hash($_REQUEST['password'],PASSWORD_DEFAULT);
+        $query = "SELECT * FROM `usuario` WHERE usuario='$username'and password='$password'";
+        $result = mysqli_query($conexion,$query) or die(mysql_error());
+        $rows = mysqli_num_rows($result);
+            if($rows==1){
+                            
+                        $_SESSION['username'] = $username;
+                        header("Location: dashboard.php");       
+                      
+                    }else{
+                        echo "<h3><b>La combinación entre nombre de usuario y cuenta no existe.</b</h3>";
+                        header( "refresh:3;url=login.php");
+                    }
+              
+            }        
+            
+        }
+
+
 }
