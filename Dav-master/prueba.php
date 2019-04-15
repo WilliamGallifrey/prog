a5fbf60e9d6ef5e82c865d72a38a615c7cf42827
davidvic99 <43604557+davidvic99@users.noreply.github.com>
2019-04-15 12:47:40 +0200

    FUNCIONANDO

--- Dav-master/prueba.php
+++ Dav-master/prueba.php
@@ -1,11 +1,17 @@
 <?php
-
+session_start();
 require('test.php');
+require('usuario.php');
 $test = new test();
 $conexion=$test->getConexion();
+$usuario = new usuario();
+$usuario->getUsuarioOnline();
+
 
 if (!empty($_POST)) {
     $corregirTest = $test->corregirTest($conexion);
+    echo "Registrado Correctamente.";
+    
     
 }else {
     $generarTest = $test->generarTest($conexion);
