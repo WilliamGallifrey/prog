e97057613600afd9c5225baf843818a805924f85
davidvic99 <43604557+davidvic99@users.noreply.github.com>
2019-04-15 10:21:53 +0200

    Funcionando Login y registro

--- Dav-master/usuario.php
+++ Dav-master/usuario.php
@@ -34,26 +34,32 @@ class usuario extends conexion{
 
     public function loginUsuario($conexion){
 
-    if (isset($_POST['usuario'])){
-        $username = stripslashes($_REQUEST['usuario']);
-        $username = mysqli_real_escape_string($conexion,$username);
-        $password = password_hash($_REQUEST['password'],PASSWORD_DEFAULT);
-        $query = "SELECT * FROM `usuario` WHERE usuario='$username'and password='$password'";
-        $result = mysqli_query($conexion,$query) or die(mysql_error());
-        $rows = mysqli_num_rows($result);
-            if($rows==1){
-                            
-                        $_SESSION['username'] = $username;
-                        header("Location: dashboard.php");       
-                      
-                    }else{
-                        echo "<h3><b>La combinación entre nombre de usuario y cuenta no existe.</b</h3>";
-                        header( "refresh:3;url=login.php");
-                    }
-              
-            }        
+        if(isset($_POST['username'])&&isset($_POST['password'])){
+            $usuario = trim($_POST['username']);
+            $password = trim($_POST['password']);
+            $sql = "SELECT * FROM usuario WHERE usuario = '".$usuario."'";
+            $result = mysqli_query($conexion,$sql);
+            $numRows = mysqli_num_rows($result);
             
+            if($numRows  == 1){
+                $row = mysqli_fetch_assoc($result);
+                if(password_verify($password,$row['password'])){
+                    echo "Logeado Correctamente";
+                    $_SESSION['username'] = $usuario;
+                }
+                else{
+                    echo "Contraseña incorrecta";
+                }
+            }
+            else{
+                echo "No existe una cuenta con ese usuario";
+            }
         }
+         
+
+    } 
+
+    
 
 
 }
